2e260604cfe96b950eef020443ab71ab42b17da3
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-11 10:58:00 +0100

    adding storage accounts for transactions updates

---
+++ azure/transactions.tf
@@ -0,0 +1,7 @@
+resource "azurerm_storage_account" "transactions" {
+  name                     = "transactions-function"
+  resource_group_name      = "${azurerm_resource_group.kiroku.name}"
+  location                 = "${azurerm_resource_group.kiroku.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
