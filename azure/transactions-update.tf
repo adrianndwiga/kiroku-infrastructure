2632d9f4df936073a4539820ca01a332cf8f18bd
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-11 11:12:43 +0100

    adding transactions update storage queue

--- azure/transactions-update.tf
+++ azure/transactions-update.tf
@@ -25,3 +25,10 @@ resource "azurerm_function_app" "transactions-update" {
   app_service_plan_id       = "${azurerm_app_service_plan.transactions-update.id}"
   storage_connection_string = "${azurerm_storage_account.transactions-update.primary_connection_string}"
 }
+
+
+resource "azure_storage_queue" "transactions-update-storage-queue" {
+  name                 = "transactions-update-storage-queue"
+  resource_group_name  = "${azurerm_resource_group.kiroku.name}"
+  storage_account_name = "${azurerm_storage_account.transactions-update.name}"
+}
