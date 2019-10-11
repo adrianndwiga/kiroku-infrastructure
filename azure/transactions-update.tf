7c64b20dafd7626d9636b8574b66527601135b2d
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-11 11:08:37 +0100

    adding transactions update azure function

--- azure/transactions-update.tf
+++ azure/transactions-update.tf
@@ -17,3 +17,11 @@ resource "azurerm_app_service_plan" "transactions-update" {
     size = "Y1"
   }
 }
+
+resource "azurerm_function_app" "transactions-update" {
+  name                      = "transactions-update-function"
+  location                  = "${azurerm_resource_group.kiroku.location}"
+  resource_group_name       = "${azurerm_resource_group.kiroku.name}"
+  app_service_plan_id       = "${azurerm_app_service_plan.transactions-update.id}"
+  storage_connection_string = "${azurerm_storage_account.transactions-update.primary_connection_string}"
+}
