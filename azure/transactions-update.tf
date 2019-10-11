6488113cf2a27ed3da1ec4444b305e8f8c7290ba
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-11 10:59:35 +0100

    renamed transaction update

--- azure/transactions-update.tf
+++ azure/transactions-update.tf
@@ -1,5 +1,5 @@
-resource "azurerm_storage_account" "transactions" {
-  name                     = "transactions-function"
+resource "azurerm_storage_account" "transactions-update" {
+  name                     = "transactions-update-function"
   resource_group_name      = "${azurerm_resource_group.kiroku.name}"
   location                 = "${azurerm_resource_group.kiroku.location}"
   account_tier             = "Standard"
