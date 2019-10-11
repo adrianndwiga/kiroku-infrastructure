60a689827761d01c1e30817bb114a83c827a313c
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-11 11:24:53 +0100

    configuring app service plan for sales and expences report

--- azure/sales-and-expences-report.tf
+++ azure/sales-and-expences-report.tf
@@ -5,3 +5,15 @@ resource "azurerm_storage_account" "sales-and-expences-report" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_app_service_plan" "sales-and-expences-report" {
+  name                = "azure-functions-sales-and-expences-report-service-plan"
+  location            = "${azurerm_resource_group.kiroku.location}"
+  resource_group_name = "${azurerm_resource_group.kiroku.name}"
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
