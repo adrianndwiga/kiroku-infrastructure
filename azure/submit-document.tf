60e8080dcc308e6f49db39f8e05409622fbbff8e
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-12 13:51:28 +0100

    adding submit document

---
+++ azure/submit-document.tf
@@ -0,0 +1,7 @@
+resource "azurerm_storage_account" "submit-document" {
+  name                     = "submit-document-function"
+  resource_group_name      = "${azurerm_resource_group.kiroku.name}"
+  location                 = "${azurerm_resource_group.kiroku.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
