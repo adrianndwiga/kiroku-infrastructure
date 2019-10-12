bed56b8cc9fcdb1bc6bba2f79f3d936158920526
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-12 14:35:28 +0100

    adding transactions update function app settings

--- azure/transactions-update.tf
+++ azure/transactions-update.tf
@@ -24,6 +24,13 @@ resource "azurerm_function_app" "transactions-update" {
   resource_group_name       = "${azurerm_resource_group.kiroku.name}"
   app_service_plan_id       = "${azurerm_app_service_plan.transactions-update.id}"
   storage_connection_string = "${azurerm_storage_account.transactions-update.primary_connection_string}"
+
+  app_settings {
+    "FUNCTIONS_WORKER_RUNTIME"  = "dotnet"
+    "FUNCTION_APP_EDIT_MODE"    = "readonly"
+    "HASH"                      = "${base64sha256(file("./dummy.zip"))}"
+    "WEBSITE_RUN_FROM_PACKAGE"  = "https://${azurerm_storage_account.transactions-update.name}.blob.core.windows.net/${azurerm_storage_container.sales-azurerm_storage_account.transactions-update.name}"
+  }
 }
 
 resource "azurerm_storage_container" "transactions-update" {
