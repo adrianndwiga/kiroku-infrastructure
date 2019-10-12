3fed91a12c33dd7ba6beb3791b56790ba7b8af65
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-12 14:28:21 +0100

    configuring sales and expences report function app settings

--- azure/sales-and-expences-report.tf
+++ azure/sales-and-expences-report.tf
@@ -24,4 +24,11 @@ resource "azurerm_function_app" "sales-and-expences-report" {
   resource_group_name       = "${azurerm_resource_group.kiroku.name}"
   app_service_plan_id       = "${azurerm_app_service_plan.sales-and-expences-report.id}"
   storage_connection_string = "${azurerm_storage_account.sales-and-expences-report.primary_connection_string}"
+  
+  app_settings {
+    "FUNCTIONS_WORKER_RUNTIME"  = "dotnet"
+    "FUNCTION_APP_EDIT_MODE"    = "readonly"
+    "HASH"                      = "${base64sha256(file("./dummy.zip"))}"
+    "WEBSITE_RUN_FROM_PACKAGE"  = "https://${azurerm_storage_account.sales-and-expences-report.name}.blob.core.windows.net/${azurerm_storage_container.sales-azurerm_storage_account.sales-and-expences-report.name}"
+  }
 }
