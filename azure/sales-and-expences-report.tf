2976b47458f7b72fd396a2a1c9461976e56b3ae3
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-13 13:55:21 +0100

    prefixing name with storage account, not sure whether this is the correct approach to prefix the name as the resource type is different

--- azure/sales-and-expences-report.tf
+++ azure/sales-and-expences-report.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "sales-and-expences-report" {
-  name                     = "sales-and-expences-report-function"
+  name                     = "sales-and-expences-report-storage-account"
   resource_group_name      = "${azurerm_resource_group.kiroku.name}"
   location                 = "${azurerm_resource_group.kiroku.location}"
   account_tier             = "Standard"
