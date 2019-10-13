e15b691e132997c85736d9a79e393eaa9c1554b4
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-13 13:14:25 +0100

    prefixing name with storage account, not sure whether this is the correct approach to prefix the name as the resource type is different

--- azure/transactions-update.tf
+++ azure/transactions-update.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "transactions-update" {
-  name                     = "transactions-update-function"
+  name                     = "transactions-update-storage-account"
   resource_group_name      = "${azurerm_resource_group.kiroku.name}"
   location                 = "${azurerm_resource_group.kiroku.location}"
   account_tier             = "Standard"
