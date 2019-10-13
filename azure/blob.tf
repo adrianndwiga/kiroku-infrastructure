c00f360da3ba934cd457f88e541a39d1d74aaab6
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-13 14:23:53 +0100

    dont need kiroku blob

--- azure/blob.tf
+++ azure/blob.tf
@@ -12,12 +12,3 @@ resource "azurerm_storage_container" "kiroku" {
   storage_account_name  = "${azurerm_storage_account.kiroku.name}"
   container_access_type = "private"
 }
-
-resource "azurerm_storage_blob" "kiroku" {
-  name                   = "dummy.zip"
-  resource_group_name    = "${azurerm_resource_group.kiroku.name}"
-  storage_account_name   = "${azurerm_storage_account.kiroku.name}"
-  storage_container_name = "${azurerm_storage_container.kiroku.name}"
-  type                   = "blob"
-  source                 = "dummy.zip"
-}
