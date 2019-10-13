75baec659a0e5c9336181a85a13ecee8b4bded19
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-10-13 10:27:50 +0100

    adding tag document configuration-added storage account

---
+++ azure/tag-document.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "tag-document" {
+  name                     = "tag-document-function"
+  resource_group_name      = "${azurerm_resource_group.kiroku.name}"
+  location                 = "${azurerm_resource_group.kiroku.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
